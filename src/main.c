b5547f57a20e33dd5c64ccafc3d0bdb21d34d415
Stefan <Stefan@ee10a7f7-9cf9-a14b-bfb7-abe9ac1a41d2>
2015-08-01 14:45:42 +0000

    [v0.3]  * Removed Sample image, both from code and resources  * Tried rotate the image, but I think it became to small (code still there, #ifdef'ed)
    Added Features:
     * View Spiral
    
    Fixed Bug:
     * Time doesn't show when the 5x5 box is white
       (Changed color from white to black)

--- src/main.c
+++ src/main.c
@@ -8,15 +8,55 @@ static BitmapLayer *s_bitmap_layer;
 static Layer *s_grid_layer;
 static TextLayer *s_time_layer;
 
-// Bitmaps
-static GBitmap *s_sample_bitmap;
+#if 1
+  // GRects (Vertical)
+  static struct GRect s_3x3_square  = {{ 21,  3}, { 61,  61}}; // 3x3 Square
+  static struct GRect s_2x2_square  = {{ 81,  3}, { 41,  41}}; // 2x2 Square
+  static struct GRect s_1x1L_square = {{ 81, 43}, { 21,  21}}; // 1x1 Square (left)
+  static struct GRect s_1x1R_square = {{101, 43}, { 21,  21}}; // 1x1 Square (right)
+  static struct GRect s_5x5_square  = {{ 21, 63}, {101, 101}}; // 5x5 Square
+  static struct GRect s_outline     = {{ 19,  1}, {105, 165}};
+#else
+  // GRects (Horizontal)
+  static struct GRect s_3x3_square  = {{ 3,  75}, { 52,  52}}; // 3x3 Square
+  static struct GRect s_2x2_square  = {{ 3,  41}, { 35,  35}}; // 2x2 Square
+  static struct GRect s_1x1L_square = {{ 37, 58}, { 18,  18}}; // 1x1 Square (bottom)
+  static struct GRect s_1x1R_square = {{ 37, 41}, { 18,  18}}; // 1x1 Square (top)
+  static struct GRect s_5x5_square  = {{ 54, 41}, { 86,  86}}; // 5x5 Square
+  static struct GRect s_outline     = {{  1, 39}, {141,  90}};
+#endif
 
-// GRects
-static struct GRect s_3x3_square  = {{ 21,  3}, { 61,  61}}; // 3x3 Square
-static struct GRect s_2x2_square  = {{ 81,  3}, { 41,  41}}; // 2x2 Square
-static struct GRect s_1x1L_square = {{ 81, 43}, { 21,  21}}; // 1x1 Square (left)
-static struct GRect s_1x1R_square = {{101, 43}, { 21,  21}}; // 1x1 Square (right)
-static struct GRect s_5x5_square  = {{ 21, 63}, {101, 101}}; // 5x5 Square
+// GPath
+static GPath *s_spiral_path_ptr = NULL;
+static const GPathInfo SPIRAL_PATH_INFO = {
+  .num_points = 24,
+  .points = (GPoint []) {
+    { 82,  43}, // (1L)
+    { 85,  54}, // (1L)
+    { 91,  59}, // (1L)
+    {101,  62}, // (1L), (1R)
+    {111,  60}, // (1R)
+    {117,  54}, // (1R)
+    {120,  44}, // (1R), (2)
+    {118,  29}, // (2)
+    {110,  17}, // (2)
+    {100,   9}, // (2)
+    { 91,   6}, // (2)
+    { 81,   4}, // (2), (3)
+    { 62,   9}, // (3)
+    { 45,  18}, // (3)
+    { 31,  33}, // (3)
+    { 25,  47}, // (3)
+    { 22,  63}, // (3), (5)
+    { 25,  86}, // (5)
+    { 32, 107}, // (5)
+    { 44, 125}, // (5)
+    { 61, 142}, // (5)
+    { 77, 151}, // (5)
+    { 96, 159}, // (5)
+    {121, 162}, // (5)
+  }
+};
 
 // Global time
 static int s_curr_hour;
@@ -33,7 +73,6 @@ static void update_time() {
   text_layer_set_text(s_time_layer, time_str);
 }
 
-// These layer update callbacks are all called on render updates
 static void grid_layer_update_callback(Layer *layer, GContext *ctx) {
 
   GColor fill_color[] = {GColorWhite, GColorRed, GColorGreen, GColorBlue};
@@ -77,37 +116,27 @@ static void grid_layer_update_callback(Layer *layer, GContext *ctx) {
 
   // Draw the grid dots
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  for (int16_t y = s_3x3_square.origin.y; y <= s_1x1L_square.size.h * 8; y += s_1x1L_square.size.h - 1) {
-    for (int16_t x = s_3x3_square.origin.x; x <= s_1x1L_square.size.w * 6; x += s_1x1L_square.size.w - 1) {
+  for (int16_t y = s_outline.origin.y + 2; y <= s_outline.origin.y + s_outline.size.h; y += s_1x1L_square.size.h - 1) {
+    for (int16_t x = s_outline.origin.x + 2; x <= s_outline.origin.x + s_outline.size.w; x += s_1x1L_square.size.w - 1) {
       graphics_draw_pixel(ctx, GPoint(x, y));
     }
   }
   
+  // Draw spiral
+  graphics_context_set_stroke_color(ctx, GColorDarkGray);
+  graphics_context_set_stroke_width(ctx, 2);
+  gpath_draw_outline_open(ctx, s_spiral_path_ptr);
+  
   // Draw the outline
   graphics_context_set_stroke_color(ctx, GColorDarkGray);
   graphics_context_set_stroke_width(ctx, 1);
-  graphics_draw_rect(ctx, GRect(19,1,105,165));
+  graphics_draw_rect(ctx, s_outline);
 }
 
 static void main_window_load(Window *window) {
   // Get windows root layer and its bounds
   Layer *root_layer = window_get_root_layer(window);
   GRect root_bounds = layer_get_frame(root_layer);
-
-#if 0
-  // *********************** SHOULD BE REMOVED! ***********************
-  // This is just to display the sample images, this and its resource
-  // should be deleted from the application later on to save space.
-  // ******************************************************************
-
-  // Create bitmap layer
-  s_sample_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SAMPLE);
-  s_bitmap_layer = bitmap_layer_create(root_bounds);
-  bitmap_layer_set_bitmap(s_bitmap_layer, s_sample_bitmap);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(root_layer, bitmap_layer_get_layer(s_bitmap_layer));
-#endif
 
   // Create the grid layer
   s_grid_layer = layer_create(root_bounds);
@@ -116,10 +145,13 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(root_layer, s_grid_layer);
 
+  // Create spiral path
+  s_spiral_path_ptr = gpath_create(&SPIRAL_PATH_INFO);
+  
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(root_bounds.origin.x, 65, root_bounds.size.w, 50));
+  s_time_layer = text_layer_create(GRect(root_bounds.origin.x, 65, root_bounds.size.w, 40));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
 
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
@@ -134,8 +166,9 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  gpath_destroy(s_spiral_path_ptr);
+  layer_destroy(s_grid_layer);
   bitmap_layer_destroy(s_bitmap_layer);
-  gbitmap_destroy(s_sample_bitmap);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
