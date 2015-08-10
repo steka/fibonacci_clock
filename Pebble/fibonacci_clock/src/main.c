4917f1b21d6f96fffa26d4d11092a8f7d4bbed1a
Stefan <Stefan@ee10a7f7-9cf9-a14b-bfb7-abe9ac1a41d2>
2015-08-10 22:22:17 +0000

    [v0.7]  * Added bluetooth status (with vibe on disconnect)  * Time (HH:MM) can now be show on tab.  * The color for time (HH:MM) can now be selected as "legible".

--- Pebble/fibonacci_clock/src/main.c
+++ Pebble/fibonacci_clock/src/main.c
@@ -14,6 +14,7 @@ static Layer *s_arc3x3_layer;
 static Layer *s_arc2x2_layer;
 static Layer *s_arc1x1L_layer;
 static Layer *s_arc1x1R_layer;
+static Layer *s_bluetooth_layer;
 
 static void initialise_ui(void) {
   s_window = window_create();
@@ -21,12 +22,12 @@ static void initialise_ui(void) {
   #ifndef PBL_SDK_3
     window_set_fullscreen(s_window, true);
   #endif
-  
+
   s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
   // s_fibo_layer
   s_fibo_layer = layer_create(GRect(19, 1, 105, 165));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_fibo_layer);
-  
+
   // s_time_layer
   s_time_layer = text_layer_create(GRect(0, 65, 144, 40));
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -34,30 +35,34 @@ static void initialise_ui(void) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_font(s_time_layer, s_res_bitham_30_black);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_time_layer);
-  
+
   // s_charge_layer
   s_charge_layer = layer_create(GRect(127, 142, 15, 22));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_charge_layer);
-  
+
   // s_arc5x5_layer
   s_arc5x5_layer = layer_create(GRect(21, 63, 101, 101));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_arc5x5_layer);
-  
+
   // s_arc3x3_layer
   s_arc3x3_layer = layer_create(GRect(21, 3, 61, 61));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_arc3x3_layer);
-  
+
   // s_arc2x2_layer
   s_arc2x2_layer = layer_create(GRect(81, 3, 41, 41));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_arc2x2_layer);
-  
+
   // s_arc1x1L_layer
   s_arc1x1L_layer = layer_create(GRect(81, 43, 21, 21));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_arc1x1L_layer);
-  
+
   // s_arc1x1R_layer
   s_arc1x1R_layer = layer_create(GRect(101, 43, 21, 21));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_arc1x1R_layer);
+
+  // s_bluetooth_layer
+  s_bluetooth_layer = layer_create(GRect(0, 141, 18, 22));
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_bluetooth_layer);
 }
 
 static void destroy_ui(void) {
@@ -70,9 +75,10 @@ static void destroy_ui(void) {
   layer_destroy(s_arc2x2_layer);
   layer_destroy(s_arc1x1L_layer);
   layer_destroy(s_arc1x1R_layer);
+  layer_destroy(s_bluetooth_layer);
 }
 // END AUTO-GENERATED UI CODE
- 
+
 #if 1
   // GRects (Vertical)
   static struct GRect s_3x3_square  = {{ 2,  2}, { 61,  61}}; // 3x3 Square
@@ -103,13 +109,15 @@ static void destroy_ui(void) {
 static GBitmap *s_battery_image;
 static GBitmap *s_charging_image;
 static GBitmap *s_plugged_image;
-  
+static GBitmap *s_bluetooth_image;
+static GBitmap *s_cross_image;
+
 // Global time
 static int s_curr_hour;
 static int s_curr_min;
 
 // Settings
-enum {
+enum {  // DON'T CHANGE THE ORDER OF THESES!!!
   ConfShowTime = 1,
   ConfShowDate,
   ConfShowMonth,
@@ -126,16 +134,20 @@ enum {
   ConfGridColor,
   ConfDotColor,
   ConfTimeColor,
-  ConfOutlineColor
+  ConfOutlineColor,
+  ConfVibeBluetooth,
+  ConfShowTimeOnTap // Does not come from config page as separate key (included in ConfShowTime)
 };
 
 static struct {
   bool showTime;
+  bool showTimeOnTap;
   bool showDate;
   bool showMonth;
   bool showWeekNum;
   bool showCharge;
   bool showBluetooth;
+  bool vibeBluetooth;
   bool showSpiral;
   GColor hourColor;
   GColor minuteColor;
@@ -147,13 +159,16 @@ static struct {
   GColor dotColor;
   GColor timeColor;
   GColor outlineColor;
+  bool legibleText; // Use legible color instead of timeColor
 } conf = {  // Set up default value
   .showTime        = false,
+  .showTimeOnTap = false,
   .showDate        = false,
   .showMonth       = false,
   .showWeekNum     = false,
   .showCharge      = true,
-  .showBluetooth   = false,
+  .showBluetooth   = true,
+  .vibeBluetooth   = true,
   .showSpiral      = true,
   .hourColor       = {.argb = GColorRedARGB8},
   .minuteColor     = {.argb = GColorGreenARGB8},
@@ -165,6 +180,7 @@ static struct {
   .dotColor        = {.argb = GColorWhiteARGB8},
   .timeColor       = {.argb = GColorPictonBlueARGB8},
   .outlineColor    = {.argb = GColorDarkGrayARGB8},
+  .legibleText = true,
 };
 
 static void handle_window_unload(Window* window) {
@@ -181,10 +197,13 @@ static void update_time() {
 #if USE_FIXED_TIME_FOR_PUBLISHING_IMAGE
   strcpy(time_str, "10:30");
 #endif
-  
+
   // Update current time on the TextLayer
   text_layer_set_text_color(s_time_layer, conf.timeColor);
   text_layer_set_text(s_time_layer, time_str);
+
+  layer_set_hidden(text_layer_get_layer(s_time_layer), !conf.showTime);
+
   layer_mark_dirty(s_fibo_layer);
 }
 
@@ -219,6 +238,19 @@ static void arc5x5_layer_update_callback(Layer *layer, GContext *ctx) {
   draw_spiral_arc(ctx, GPoint(bound.size.w, 0), bound.size.w);
 }
 
+static void bluetooth_layer_update_callback(Layer *layer, GContext *ctx) {
+  bool connected = bluetooth_connection_service_peek();
+
+  // Draw bluetooth icon
+  graphics_context_set_compositing_mode(ctx, GCompOpSet);
+  graphics_draw_bitmap_in_rect(ctx, s_bluetooth_image, gbitmap_get_bounds(s_bluetooth_image));
+
+  // Draw cross if not connected
+  if (!connected) {
+    graphics_draw_bitmap_in_rect(ctx, s_cross_image, gbitmap_get_bounds(s_cross_image));
+  }
+}
+
 static void fibo_layer_update_callback(Layer *layer, GContext *ctx) {
   GColor fill_color[4]; // None, Hour, Minute, Hr+Min
   int8_t fc1L = 0, fc1R = 0, fc2 = 0, fc3 = 0, fc5 = 0;
@@ -229,7 +261,7 @@ static void fibo_layer_update_callback(Layer *layer, GContext *ctx) {
   fill_color[1] = conf.hourColor;
   fill_color[2] = conf.minuteColor;
   fill_color[3] = conf.hourMinuteColor;
-  
+
 #if USE_FIXED_TIME_FOR_PUBLISHING_IMAGE
   h = 10;
   m = 30 / 5;
@@ -258,6 +290,7 @@ static void fibo_layer_update_callback(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, fill_color[fc1R]);
   graphics_fill_rect(ctx, s_1x1R_square, 0, GCornerNone);
   graphics_context_set_fill_color(ctx, fill_color[fc5]);
+  text_layer_set_text_color(s_time_layer, conf.legibleText ? gcolor_legible_over(fill_color[fc5]): conf.timeColor);
   graphics_fill_rect(ctx, s_5x5_square, 0, GCornerNone);
 
   // Draw the grid
@@ -276,7 +309,7 @@ static void fibo_layer_update_callback(Layer *layer, GContext *ctx) {
       graphics_draw_pixel(ctx, GPoint(x, y));
     }
   }
-  
+
   // Draw the outline
   graphics_context_set_stroke_color(ctx, conf.outlineColor);
   graphics_context_set_stroke_width(ctx, 1);
@@ -286,17 +319,17 @@ static void fibo_layer_update_callback(Layer *layer, GContext *ctx) {
 static void charge_layer_update_callback(Layer *layer, GContext *ctx) {
   BatteryChargeState batChargeState = battery_state_service_peek();
   GColor charge_color = GColorClear;
-  
+
   // Select color for battery charge
   if (batChargeState.charge_percent >= 60) charge_color = GColorGreen;
   else if (batChargeState.charge_percent >= 25) charge_color = GColorYellow;
   else charge_color = GColorRed;
-  
+
   // Draw battery charge
   graphics_context_set_fill_color(ctx, charge_color);
   int h = (100 - batChargeState.charge_percent) / 5 + 1;
   graphics_fill_rect(ctx, GRect(3, h, 8, 21 - h), 0, GCornerNone);
-  
+
   // Draw battery outline
   graphics_context_set_compositing_mode(ctx, GCompOpSet);
   graphics_draw_bitmap_in_rect(ctx, s_battery_image, gbitmap_get_bounds(s_battery_image));
@@ -305,7 +338,7 @@ static void charge_layer_update_callback(Layer *layer, GContext *ctx) {
   if (batChargeState.is_charging) {
     graphics_draw_bitmap_in_rect(ctx, s_charging_image, gbitmap_get_bounds(s_charging_image));
   } else
-  
+
   // Draw plugged icon, if active
   if (batChargeState.is_plugged) {
     graphics_draw_bitmap_in_rect(ctx, s_plugged_image, gbitmap_get_bounds(s_plugged_image));
@@ -313,14 +346,25 @@ static void charge_layer_update_callback(Layer *layer, GContext *ctx) {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-
   s_curr_hour = (tick_time->tm_hour > 12) ? tick_time->tm_hour - 12 : tick_time->tm_hour;
   s_curr_min = tick_time->tm_min;
+
+  update_time();
 }
 
 static void battery_charge_handler(BatteryChargeState charge) {
   layer_mark_dirty(s_charge_layer);
+}
+
+static void bluetooth_handler(bool connected) {
+  if (!connected && conf.vibeBluetooth) {
+    vibes_short_pulse();
+  }
+  layer_mark_dirty(s_bluetooth_layer);
+}
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  layer_set_hidden(text_layer_get_layer(s_time_layer), false);
 }
 
 static void load_config(void) {
@@ -328,6 +372,18 @@ static void load_config(void) {
     conf.showTime = persist_read_bool(ConfShowTime);
 //    APP_LOG(APP_LOG_LEVEL_INFO, "Load config showTime = %d", conf.showTime);
     layer_set_hidden(text_layer_get_layer(s_time_layer), !conf.showTime);
+  }
+
+  if (persist_exists(ConfShowTimeOnTap)) {
+    conf.showTimeOnTap = persist_read_bool(ConfShowTimeOnTap);
+//    APP_LOG(APP_LOG_LEVEL_INFO, "Load config showTimeOnTap = %d", conf.showTimeOnTap);
+    if (conf.showTimeOnTap) {
+      accel_tap_service_subscribe(tap_handler);
+//      APP_LOG(APP_LOG_LEVEL_INFO, "Subscribe tap");
+    } else {
+      accel_tap_service_unsubscribe();
+//      APP_LOG(APP_LOG_LEVEL_INFO, "Unsubscribe tap");
+    }
   }
 
   if (persist_exists(ConfShowDate)) {
@@ -357,7 +413,12 @@ static void load_config(void) {
   if (persist_exists(ConfShowBluetooth)) {
     conf.showBluetooth = persist_read_bool(ConfShowBluetooth);
 //    APP_LOG(APP_LOG_LEVEL_INFO, "Load config showBluetooth = %d", conf.showBluetooth);
-    // Todo
+    layer_set_hidden(s_bluetooth_layer, !conf.showBluetooth);
+  }
+
+  if (persist_exists(ConfVibeBluetooth)) {
+    conf.vibeBluetooth = persist_read_bool(ConfVibeBluetooth);
+//    APP_LOG(APP_LOG_LEVEL_INFO, "Load config vibeBluetooth = %d", conf.vibeBluetooth);
   }
 
   if (persist_exists(ConfShowSpiral)) {
@@ -393,7 +454,7 @@ static void load_config(void) {
 //    APP_LOG(APP_LOG_LEVEL_INFO, "Load config noneColor = %d", conf.noneColor.argb);
     layer_mark_dirty(s_fibo_layer);
   }
-   
+
   if (persist_exists(ConfSpiralColor)) {
     conf.spiralColor.argb = persist_read_int(ConfSpiralColor);
 //    APP_LOG(APP_LOG_LEVEL_INFO, "Load config spiralColor = %d", conf.spiralColor.argb);
@@ -423,7 +484,9 @@ static void load_config(void) {
   }
 
   if (persist_exists(ConfTimeColor)) {
-    conf.timeColor.argb = persist_read_int(ConfTimeColor);
+    int value = persist_read_int(ConfTimeColor);
+    conf.legibleText = (value == 256);
+    conf.timeColor.argb = (uint8_t)(value & 0xFF);
 //    APP_LOG(APP_LOG_LEVEL_INFO, "Load config timeColor = %d", conf.timeColor.argb);
     update_time();
   }
@@ -444,12 +507,34 @@ static void inbox_received_handler(DictionaryIterator *iterator, void *context) 
 //    } else {
 //      APP_LOG(APP_LOG_LEVEL_INFO, "RxTuple (unknown type)");
 //    }
-    
+
     switch(t->key) {
       case ConfShowTime:
-        conf.showTime = atoi(t->value->cstring);
+        switch (atoi(t->value->cstring)) {
+          default:
+          case 0: // Hide
+            conf.showTime = false;
+            conf.showTimeOnTap = false;
+          break;
+          case 1: // Show
+            conf.showTime = true;
+            conf.showTimeOnTap = false;
+          break;
+          case 2: // Show on tap
+            conf.showTime = false;
+            conf.showTimeOnTap = true;
+          break;
+        }
         persist_write_bool(t->key, conf.showTime);
+        persist_write_bool(ConfShowTimeOnTap, conf.showTimeOnTap);
         layer_set_hidden(text_layer_get_layer(s_time_layer), !conf.showTime);
+        if (conf.showTimeOnTap) {
+          accel_tap_service_subscribe(tap_handler);
+//          APP_LOG(APP_LOG_LEVEL_INFO, "Subscribe tap");
+        } else {
+          accel_tap_service_unsubscribe();
+//          APP_LOG(APP_LOG_LEVEL_INFO, "Unsubscribe tap");
+        }
         break;
       case ConfShowDate:
         conf.showDate = atoi(t->value->cstring);
@@ -474,7 +559,11 @@ static void inbox_received_handler(DictionaryIterator *iterator, void *context) 
       case ConfShowBluetooth:
         conf.showBluetooth = atoi(t->value->cstring);
         persist_write_bool(t->key, conf.showBluetooth);
-        // Todo
+        layer_set_hidden(s_bluetooth_layer, !conf.showBluetooth);
+        break;
+      case ConfVibeBluetooth:
+        conf.vibeBluetooth = atoi(t->value->cstring);
+        persist_write_bool(t->key, conf.vibeBluetooth);
         break;
       case ConfShowSpiral:
         conf.showSpiral = atoi(t->value->cstring);
@@ -503,7 +592,7 @@ static void inbox_received_handler(DictionaryIterator *iterator, void *context) 
       case ConfNoneColor:
         conf.noneColor = (GColor){.argb = atoi(t->value->cstring)};
         persist_write_int(t->key, conf.noneColor.argb);
-        layer_mark_dirty(s_fibo_layer);        
+        layer_mark_dirty(s_fibo_layer);
         break;
       case ConfSpiralColor:
         conf.spiralColor = (GColor){.argb = atoi(t->value->cstring)};
@@ -529,11 +618,13 @@ static void inbox_received_handler(DictionaryIterator *iterator, void *context) 
         persist_write_int(t->key, conf.dotColor.argb);
         layer_mark_dirty(s_fibo_layer);
         break;
-      case ConfTimeColor:
-        conf.timeColor = (GColor){.argb = atoi(t->value->cstring)};
-        persist_write_int(t->key, conf.timeColor.argb);
+      case ConfTimeColor: {
+        int value = atoi(t->value->cstring);
+        conf.legibleText = (value == 256);
+        conf.timeColor = (GColor){.argb = (uint8_t)(value & 0xFF)};
+        persist_write_int(t->key, value);
         update_time();
-        break;
+      } break;
       case ConfOutlineColor:
         conf.outlineColor = (GColor){.argb = atoi(t->value->cstring)};
         persist_write_int(t->key, conf.outlineColor.argb);
@@ -550,7 +641,7 @@ void show_main(void) {
     .unload = handle_window_unload,
   });
   window_stack_push(s_window, true);
-  
+
   // Set up layer callbacks
   layer_set_update_proc(s_fibo_layer, fibo_layer_update_callback);
   layer_set_update_proc(s_charge_layer, charge_layer_update_callback);
@@ -559,18 +650,24 @@ void show_main(void) {
   layer_set_update_proc(s_arc1x1L_layer, arc1x1L_layer_update_callback);
   layer_set_update_proc(s_arc1x1R_layer, arc1x1R_layer_update_callback);
   layer_set_update_proc(s_arc5x5_layer, arc5x5_layer_update_callback);
-  
+  layer_set_update_proc(s_bluetooth_layer, bluetooth_layer_update_callback);
+
   // Create bitmaps
   s_battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY);
   s_charging_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CHARGING);
   s_plugged_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PLUGGED);
-  
+  s_bluetooth_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH);
+  s_cross_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CROSS);
+
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
+
   // Register with BatteryStateService
   battery_state_service_subscribe(battery_charge_handler);
-  
+
+  // Register with BluetoothConnectionService
+  bluetooth_connection_service_subscribe(bluetooth_handler);
+
   app_message_register_inbox_received((AppMessageInboxReceived) inbox_received_handler);
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
   load_config();
@@ -585,9 +682,12 @@ void hide_main(void) {
   gbitmap_destroy(s_battery_image);
   gbitmap_destroy(s_charging_image);
   gbitmap_destroy(s_plugged_image);
+  gbitmap_destroy(s_bluetooth_image);
+  gbitmap_destroy(s_cross_image);
 
   tick_timer_service_unsubscribe();
   battery_state_service_unsubscribe();
+  bluetooth_connection_service_unsubscribe();
 }
 
 int main(void) {
